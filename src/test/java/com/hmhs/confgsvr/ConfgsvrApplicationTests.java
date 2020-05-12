069f68500cbe5d91796078f3ea4360dcb6998cf8
Yogesh Sharma <yogesh@Yogeshs-MacBook-Pro.local>
2020-05-11 22:30:14 -0400

    first commit

---
+++ src/test/java/com/hmhs/confgsvr/ConfgsvrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hmhs.confgsvr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfgsvrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
